d3a78170994e3f737e8e0864354994f2c98fdb6f
Alejandro Nanez <alejonanez@gmail.com>
2016-11-05 22:54:34 -0500

    MVP

--- src/App.js
+++ src/App.js
@@ -1,27 +1,54 @@
 import React, { Component } from 'react';
-import Inputs from './Inputs';
 import Contacts from './Contacts';
 
 class App extends Component {
+
+  state = {
+    contactsList: []
+  }
+
+  onFormSubmit(event) {
+    event.preventDefault();
+
+    const contactName = event.target.querySelector('#contactName').value;
+    const contactPhone = event.target.querySelector('#contactPhone').value;
+    const newContactsList = this.state.contactsList;
+
+    // newContactsList => []
+    // this.state.contactsList => []
+
+    newContactsList.push({
+      name: contactName,
+      phone: contactPhone
+    });
+
+    // newContactsList => [{name: 'alenandrp', phone: 123321 }]
+    // this.state.contactsList => []
+
+    this.setState({
+      contactsList: newContactsList
+    });
+
+    // this.state.contactsList => [{name: 'alejandro', phone: 123321 }]
+  }
+
   render() {
-  	var contacts = [
-  		{
-	  		name: 'Angela',
-	  		phone: 12345
-  		},
-  		{
-	  		name: 'Angela',
-	  		phone: 12345
-  		},
-  		{
-	  		name: 'Angela',
-	  		phone: 12345
-  		}
-  	];
     return (
       <div>
-      	<Inputs />
-      	<Contacts contacts={contacts}/>
+        <div className="row">
+          <form onSubmit={(event) => this.onFormSubmit(event)}>
+            <div className="medium-4 columns"> 
+              <input type="text" placeholder="Contact's Name" id="contactName"/>
+            </div>
+            <div className="medium-4 columns">        
+              <input type="text" placeholder="Contact's Phone" id="contactPhone"/>
+            </div>     
+              <div className="medium-4 columns">
+              <button type="submit" className="success button">Add Contact</button>
+            </div>
+          </form>
+        </div>
+        <Contacts contacts={this.state.contactsList}/>
       </div>
     );
   }
--- src/Contacts.js
+++ src/Contacts.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 
 class Contacts extends Component {
   render() {
-  	var contactsList = this.props.contacts.map(contact => <li>{contact.name} {contact.phone}</li>);
+  	var contactsList = this.props.contacts.map(contact => <li key={contact.phone}>{contact.name} {contact.phone}</li>);
     return (
 	    <div className="row">
 	    	<div className="medium-4 medium-offset-4">
